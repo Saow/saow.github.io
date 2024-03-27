fcba03ac6e93c57c92bed38e8db9f926a0a2fbc7
saow <samuelnummela06@gmail.com>
2024-03-27 21:57:10 +0200

    projects commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('#textswitch', {
+        strings: ['Fullstack developer', 'Web developer', 'UI/UX designer'],
+        typeSpeed: 50,
+        backSpeed: 30,
+        loop: true
+    });
+});
